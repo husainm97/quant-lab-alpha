5a1d7c8216b1855268d1c120946142f4fd0473e1
husainm97 <37531806+husainm97@users.noreply.github.com>
2025-08-03 00:42:20 +0200

    Create fetch_ff5.py

---
+++ data/fetch_ff5.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import zipfile
+import io
+import requests
+
+def fetch_ff5_monthly():
+    url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_5_Factors_2x3_CSV.zip"
+    r = requests.get(url)
+    with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+        with z.open(z.namelist()[0]) as f:
+            df = pd.read_csv(f, skiprows=3)
+    return df
