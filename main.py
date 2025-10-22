3e55470557f5426eef3ee13d668d87be1c6477b9
Husain Manasawala <37531806+husainm97@users.noreply.github.com>
2025-10-22 23:33:49 +0200

    Add comment about main backend interface functionality
    Added a comment explaining the purpose of the main file.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.factor_model import run_factor_regression
 
+# This file should run the main backend interface to import / select the ETFs and produce an analysis about the porfolio's factor loadings 
+
 def main():
     print("Starting Fama-French Factor Regression analysis...")
   
